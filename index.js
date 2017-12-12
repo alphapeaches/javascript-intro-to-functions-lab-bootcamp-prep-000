a47e293d5467a051642cd3ddc083f7f673e0e8d3
Rachel Acosta <denversunrise@gmail.com>
2017-12-12 17:55:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase());{
     return "I can't hear you!";
   }
-    else if (string === string.toUpperCase());{
+    elseif (string === string.toUpperCase());{
       return "YES INDEED!";
     }
     else if (string === "I love you, Grandma.");{
