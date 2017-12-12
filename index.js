1965f1b708d3c263c08d684bd0a46450a7402438
Rachel Acosta <denversunrise@gmail.com>
2017-12-12 17:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string=== whisper(string){
-    return "I can't hear you"
+  if string === whisper(string){
+    return "I can't hear you!";
   };
     elseif string === shout(string){
-      return "YES INDEED"
+      return "YES INDEED!";
+    }
+    elseif string === "I love you, Grandma" {
+      return "I love you, too."
     }
 }
